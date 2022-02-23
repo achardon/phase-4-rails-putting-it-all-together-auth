a4ef7352c2db107bb24544107f1674e831704781
Alessandra Chardon <achardon@ucla.edu>
2022-02-23 16:41:15 -0500

    change error messages to arrays in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
+# rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user
     else
-      # render json: {error: user.errors.full_messages}, status: :unauthorized
-      render json: {error: "Username or Password Incorrect"}, status: :unauthorized
+      # render json: {errors: user.errors}, status: :unauthorized
+      render json: {errors: ["Username or Password Incorrect"]}, status: :unauthorized
     end
   end
 
@@ -15,7 +17,12 @@ class SessionsController < ApplicationController
       session.destroy
       head :no_content
     else
-      render json: {error: "No user logged in"}, status: :unauthorized
+      render json: {errors: ["No user logged in"]}, status: :unauthorized
     end
   end
+
+  # def record_invalid(invalid)
+  #   render json: {errors: invalid.record.errors.full_messages}, status: :unauthorized
+  # end
+
 end
