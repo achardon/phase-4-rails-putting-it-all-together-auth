5ab92a5f74c4681c80c7c33b3b6f6d3513da4484
Alessandra Chardon <achardon@ucla.edu>
2022-02-24 08:00:12 -0500

    add index to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,15 @@
 class RecipesController < ApplicationController
+    before_action :authorize
+
+    def index
+        render json: Recipe.all, status: :created
+    end
+
+
+    private
+
+    def authorize
+        return render json: {errors: ["Unauthorized"]}, status: :unauthorized unless session[:user_id]
+    end
+
 end
